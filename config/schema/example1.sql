e609700f0cc7576e247643dd8c67237ad6b9d446
nico-amsterdam <16443840+nico-amsterdam@users.noreply.github.com>
2020-10-21 12:43:04 +0200

    Database collate to get case insensitive unique keys by default.

--- config/schema/example1.sql
+++ config/schema/example1.sql
@@ -1,4 +1,5 @@
-create database example1;
+CREATE DATABASE example1 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 create user 'organizer'@'localhost';
 GRANT ALL PRIVILEGES ON example1.* To 'organizer'@'localhost' IDENTIFIED BY 'your secret password';
 
