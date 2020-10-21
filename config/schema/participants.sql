7bca84497ee1996a88a4012916f40d2282bdc8a4
nico-amsterdam <16443840+nico-amsterdam@users.noreply.github.com>
2020-10-21 13:08:25 +0200

    Hint how you can do this without CakePHP timestamp behavior

--- config/schema/participants.sql
+++ config/schema/participants.sql
@@ -4,8 +4,8 @@ CREATE TABLE participants (
  name VARCHAR(50) NOT NULL,
  email VARCHAR(255),
  date_of_birth DATETIME,
- created DATETIME,
- modified DATETIME,
+ created DATETIME,  -- or: created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ modified DATETIME, -- or: modified TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
  UNIQUE KEY contest_name_uniq (contest_id, name),
  UNIQUE KEY contest_email_uniq (contest_id, email), 
  FOREIGN KEY contest_fk (contest_id) REFERENCES contests(id)
